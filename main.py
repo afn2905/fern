1c3deb92ece6d228189be81b4d0a5bb21f16c681
AFN2905 <53172772+afn2905@users.noreply.github.com>
2023-04-09 16:12:32 +0000

    tinkering

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+DATES = [
+  {
+    'id': 1,
+    'date': '7 May 2023'
+  },
+  {
+    'id': 2,
+    'date': '13 May 2023'
+  },
+  {
+    'id': 3,
+    'date': '16 May 2023'
+  },
+  {
+    'id': 4,
+    'date': '17 May 2023'
+  },
+]
+
 @app.route("/")
 def home():
-  return render_template('home.html')
+  return render_template('home.html', dates = DATES)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
